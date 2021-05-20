5faab3ac25f59e069a99d4e8070e9b2efc75ab49
Naincy Mourya <anuragsinghbam@gmail.com>
2021-05-20 15:17:08 +0530

    add teer hunt page

--- index.php
+++ index.php
@@ -21,38 +21,62 @@ $rows=$result->fetch_assoc()
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <link href='css/style1.css?v=1.0' rel=stylesheet media=all>
     <link href='images/16.ico' rel="shortcut icon" type=image/x-icon>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/teer.css">
+    
 </head>
 
 <body>
-    <div class=header id=home>
-        <div class=top-header>
-            <a href=index.php><img alt=Teer src=images/teer.jpg title=Teer></a>
+    <header class="header">
+        <div class="header-content">
+        <div class="logo">
+            <img src="./images/teer.jpg" alt="">
+        </div>
+        <div class="date">
             <p id="date"></p>
         </div>
-        <div>
-            <marquee behavior="" direction="left">Text from admin site</marquee>
         </div>
+        <div class="news">
+        <marquee behavior="" direction="">
+            <p>Running Admin News</p>
+        </marquee>
     </div>
+    </header>
 
-    <!-- <div class=work id=work>
+    <section class="dream-numbers">
+        <h1>Today's Super Luck Teer Result</h1>
+        <div class="scores">
+            <div class="score">
+                <div class="score-title">
+                <p>1st Round (04:20 PM)</p>
+                </div>
+
+                <div class="score-card">
+                    <p>20</p>
+                </div>
+            </div>
+            <div class="timers">
+                <div class="timer">
+                    <p>1st Round Result Time</p> <p><b class="timer-time">(04:20 PM)</b></p>
+                </div>
+                <div class="timer">
+                    <p>1st Round Result Time </p> <p><b class="timer-time">(04:20 PM)</b></p>
+                </div>
+            </div>
+            <div class="score">
+            <div class="score-title">
+                <p>2nd Round (04:20 PM)</p>
+            </div>
+            <div class="score-card">
+                    <p>20</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class=work id=work>
         <div>
             <div class=container style=background-color:#fff>
-                <div class="">
-                    <h2 ></h2>
-                    <table class=table>
-                        <tr>
-                            <th colspan=2>BHUTAN TEER</th>
-                        </tr>
-                        <tr style=background-color:#3fd5ba>
-                            <td align=center>F/R(4:20PM)
-                            <td align=center>S/R(5:10PM)
-                        </tr>
-                        <tr style="background-color:cream;border:1px solid #000">
-                            <td><?php echo $rows['F/R'] ?></td>
-							<td><?php echo $rows['S/R'] ?></td>
-                    </table>
-                </div>
+               
                 <div class=work style=background-color:#fff id=work>
                     <div class=container style=background-color:#fff>
                         <div class="head-one team-head text-center"></div>
@@ -71,9 +95,6 @@ $rows=$result->fetch_assoc()
                                 <div class=caption style=display:none></div>
                             </div>
                         </div>
-                    </div>
-
-                    <div class=works>
                         <div class="col-md-45 work-grid">
                             <div class=item1><a href=previous-result.php><img alt="Teer Previous Results"
                                         src=images/teer-previous-numbers.jpg title="Previous Result"></a>
@@ -95,32 +116,13 @@ $rows=$result->fetch_assoc()
                 <div class=footer-right1><a href=privacypolicy.php>Privacy Policy</a></div>
             </div>
         </div>
-    </div> -->
-
-    <div class="container">
-        <div class="row" style="justify-content: center;">
-            <p class="h1"> Today's Super lucky Teer Result</p>
-        </div>
-        <div class="row py-3">
-            <div class="col-md-3">
-                <p class="info">1 <sup>st</sup> Round (4:15 PM)</p>
-                <div class="show">
-                    <p><?php echo $rows['F/R'] ?></p>
-                </div>
-            </div>
-            <div class="col-md-6"></div>
-            <div class="col-md-3">
-                <p class="info">2 <sup>nd</sup> Round (5:15 PM)</p>
-                <div class="show">
-                    <p><?php echo $rows['F/R'] ?></p>
-                </div>
-            </div>
-        </div>
     </div>
+    
+
+    
+
 <script src="js/my_script.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
